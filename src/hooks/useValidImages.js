0eef4f8f15fffbdf32965f0a03e5214ebd86866e
Wang Xin <xinwang.art@gmail.com>
2026-08-29 21:07:35 +0800

    Delete src/hooks directory

--- src/hooks/useValidImages.js
+++
@@ -1,9 +0,0 @@
-import { useMemo } from 'react';
-import { imageUtils } from '@/utils/imageUtils';
-export const useValidImages = (images, fallbackImage = "/no-image.png") => {
-    return useMemo(() => {
-      const filtered = images.filter(imageUtils.isValidImage);
-      return filtered.length > 0 ? filtered : [fallbackImage];
-    }, [images, fallbackImage]);
-  };
-  
